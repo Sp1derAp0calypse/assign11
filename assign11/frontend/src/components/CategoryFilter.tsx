92a02338020274437a1674541c8018bb419c3259
Sp1derAp0calypse <131923585+Sp1derAp0calypse@users.noreply.github.com>
2025-04-03 23:04:09 -0600

    Update CategoryFilter.tsx

--- assign11/frontend/src/components/CategoryFilter.tsx
+++ assign11/frontend/src/components/CategoryFilter.tsx
@@ -1,55 +1,58 @@
-import { useState, useEffect } from 'react'
-import './CategoryFilter.css'
+import { useState, useEffect } from 'react';
+import './CategoryFilter.css';
 
 function CategoryFilter({
-    selectedCategories, 
-    setSelectedCategories,
-}: { 
-    selectedCategories: string[];
-    setSelectedCategories: (categories: string[]) => void;
+  selectedCategories,
+  setSelectedCategories,
+}: {
+  selectedCategories: string[];
+  setSelectedCategories: (categories: string[]) => void;
 }) {
-    const [categories, setCategories] = useState<string[]>([])
+  const [categories, setCategories] = useState<string[]>([]);
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-            try {
-            const response = await fetch('https://localhost:5000/Book/GetBookCategories');
-            const data = await response.json()
-            console.log('shoulda worked:', data)
-            setCategories(data)
-            } catch (error) {
-              console.error("yeah idk man it broke", error)
-            }
-        };
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(
+          'https://assignment13-backend-bhfsaqdyece7hxcq.eastus-01.azurewebsites.net/Book/GetBookCategories'
+        );
+        const data = await response.json();
+        console.log('shoulda worked:', data);
+        setCategories(data);
+      } catch (error) {
+        console.error('yeah idk man it broke', error);
+      }
+    };
 
-        fetchCategories();
-    }, [])
+    fetchCategories();
+  }, []);
 
-    function handleCheckboxChange ({target}: {target: HTMLInputElement}) {
-        const updatedCategories = selectedCategories.includes(target.value)
-        ? selectedCategories.filter(c => c !== target.value)
-        : [...selectedCategories, target.value];
-        setSelectedCategories(updatedCategories);
-    }
+  function handleCheckboxChange({ target }: { target: HTMLInputElement }) {
+    const updatedCategories = selectedCategories.includes(target.value)
+      ? selectedCategories.filter((c) => c !== target.value)
+      : [...selectedCategories, target.value];
+    setSelectedCategories(updatedCategories);
+  }
 
-    return (
-        <div className='category-filter'>
-            <h5>Book Categories</h5>
-            <div className='category-list'>
-                {categories.map((c) => (
-                    <div key={c} className='category-items'>
-                        <input type="checkbox" 
-                        id={c} 
-                        value={c} 
-                        className='category-checkbox'
-                        onChange={handleCheckboxChange}
-                        />
-                        <label htmlFor={c}>{c}</label>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="category-filter">
+      <h5>Book Categories</h5>
+      <div className="category-list">
+        {categories.map((c) => (
+          <div key={c} className="category-items">
+            <input
+              type="checkbox"
+              id={c}
+              value={c}
+              className="category-checkbox"
+              onChange={handleCheckboxChange}
+            />
+            <label htmlFor={c}>{c}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CategoryFilter;
